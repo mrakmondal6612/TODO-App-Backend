682b1c10e09ba51da0746b4a446e8db57db0edd6
Ajay Mondal <mrakmondal6612@gmail.com>
2024-12-04 20:54:18 +0530

    dfgdfg

--- Backend/index.js
+++ Backend/index.js
@@ -7,13 +7,9 @@ const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://todo-app-frontend-xi-one.vercel.app/",
-  })
-);
+app.use(cors());
 app.use("/todo", router);
 
 app.listen(PORT, (req, res) => {
-    console.log(`Server is running  on PORT ${PORT}`);
+  console.log(`Server is running  on PORT ${PORT}`);
 });
